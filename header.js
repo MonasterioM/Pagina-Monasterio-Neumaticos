ced80cd0bcb73546b3f7992bbf01585e43685bcd
IsaiasC1999 <isaias.romano99@gmail.com>
2023-11-22 23:29:11 -0300

    agregad0 boton toggle

--- header.js
+++ header.js
@@ -1,4 +1,19 @@
 const btnCloseMenu = document.getElementById("menu-btn-close");
+const idButtonToggle = document.getElementById("id-btn-toggle");
+
+
+
+
+
+
+idButtonToggle.onclick = ()=>{
+
+    
+
+
+}
+
+
 
 
 
