42fd06ff16eca3c999abd7d857832e7df2b775c9
IsaiasC1999 <100697426+IsaiasC1999@users.noreply.github.com>
2023-11-23 13:00:45 -0300

    agregado de menu hamburguesa

--- header.js
+++ header.js
@@ -1,18 +1,30 @@
 const btnCloseMenu = document.getElementById("menu-btn-close");
 const idButtonToggle = document.getElementById("id-btn-toggle");
-
-
+const gg = document.getElementById('fondo-opaciti')
+const menu = document.getElementById('menu');
 
 
 
 
 idButtonToggle.onclick = ()=>{
 
-    
-
+  menu.classList.toggle('toggle')
+  gg.classList.toggle('active-div');  
 
 }
 
+
+btnCloseMenu.onclick = ()=>{
+
+  menu.classList.toggle('toggle')  
+  gg.classList.toggle('active-div')
+
+}
+
+gg.onclick = ()=>{
+  menu.classList.toggle('toggle')
+  gg.classList.toggle('active-div')
+}
 
 
 
